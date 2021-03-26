efca823507367d4d16e75c79380d917e091194b6
Vinicius <viniciusgomeshonorio@hotmail.com>
2021-03-26 08:30:57 -0300

    Exercicio VelocMedia

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercicio_VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double distancia, tempo, resultado;
+            
+            Console.Write("-------Velocidade Média-------");
+            Console.WriteLine(); 
+
+            Console.Write("Digite Distancia Percorrida: ");
+            distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Digite Tempo Gasto: ");
+            tempo = Convert.ToDouble(Console.ReadLine());
+
+
+            resultado = distancia / tempo;
+
+             Console.WriteLine($"Resultado da Velocidade Média: {resultado:N3}");
+
+
+
+
+
+        }
+    }
+}
